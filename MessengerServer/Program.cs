5608cae6d2107ef6faa90c7494b98ef68fca762d
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-05-28 15:29:19 +0200

    Base structure of the server
    Adding Sub Projects for the base funktions of the server

---
+++ MessengerServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Server
+{
+    public static class Server
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static void awaitInteraction()
+        {
+
+        }
+    }
+}
